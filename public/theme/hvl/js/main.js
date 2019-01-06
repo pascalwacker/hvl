2426685a307d3c2817347d6cc81dc709cda78d11
Pascal Wacker <pascal.wacker@gmx.ch>
2019-01-07 00:49:21 +0100

    initial commit

---
+++ public/theme/hvl/js/main.js
@@ -0,0 +1,205 @@
+/*
+	Atmosphere by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
+*/
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+		xxsmall: '(max-width: 360px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$banner = $('#banner');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Countdown
+		countdown.setLabels(
+			' Millisekunde| Sekunde| Minute| Stunde| Tag| Woche| Monat| Jahr| Jahrzehnt| Jahrjundert| Jahrtausend',
+			' Millisekunden| Sekunden| Minuten| Stunden| Tage| Wochen| Monate| Jahre| Jahrzehnte| Jahrhunderte| Jahrtausende',
+			' und ',
+			', ',
+			'',
+			function(n){ return n.toString(); });
+		$('.countdown-next-hilari').each(function() {
+			var elem = $(this);
+			if (elem.data('date') || false) {
+				countdown(Date.parse(elem.data('date')), function(ts) {
+					elem.html(ts.toHTML());
+				}, countdown.DEFAULTS, 3);
+			}
+		});
+
+		// Tabs
+		$('.tabs-trigger-wrapper').each(function() {
+			var triggers = $(this).find('[data-tabs-trigger]');
+			var targets = $($(this).data('tabs-target') + ' [data-tabs-target]');
+
+			triggers.on('click', function(e) {
+				triggers.removeClass('tabs-trigger-active');
+				$(this).addClass('tabs-trigger-active');
+				targets.hide();
+				targets.filter('[data-tabs-target="' + $(this).data('tabs-trigger') + '"]').show();
+			});
+
+			(triggers.filter('[data-tabs-trigger-default]:first') || triggers.filter(':first')).trigger('click');
+		});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Scrolly.
+			$('.scrolly').scrolly({
+				offset: function() {
+					return $header.height();
+				}
+			});
+
+		// Header.
+			if (skel.vars.IEVersion < 9)
+				$header.removeClass('alt');
+
+			if ($banner.length > 0
+			&&	$header.hasClass('alt')) {
+
+				$window.on('resize', function() { $window.trigger('scroll'); });
+
+				$banner.scrollex({
+					bottom:		$header.outerHeight(),
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt'); },
+					leave:		function() { $header.removeClass('alt'); }
+				});
+
+			}
+
+		// Menu.
+			var $menu = $('#menu');
+
+			$menu._locked = false;
+
+			$menu._lock = function() {
+
+				if ($menu._locked)
+					return false;
+
+				$menu._locked = true;
+
+				window.setTimeout(function() {
+					$menu._locked = false;
+				}, 350);
+
+				return true;
+
+			};
+
+			$menu._show = function() {
+
+				if ($menu._lock())
+					$body.addClass('is-menu-visible');
+
+			};
+
+			$menu._hide = function() {
+
+				if ($menu._lock())
+					$body.removeClass('is-menu-visible');
+
+			};
+
+			$menu._toggle = function() {
+
+				if ($menu._lock())
+					$body.toggleClass('is-menu-visible');
+
+			};
+
+			$menu
+				.appendTo($body)
+				.on('click', function(event) {
+
+					event.stopPropagation();
+
+					// Hide.
+						$menu._hide();
+
+				})
+				.find('.inner')
+					.on('click', '.close', function(event) {
+
+						event.preventDefault();
+						event.stopPropagation();
+						event.stopImmediatePropagation();
+
+						// Hide.
+							$menu._hide();
+
+					})
+					.on('click', function(event) {
+						event.stopPropagation();
+					})
+					.on('click', 'a', function(event) {
+
+						var href = $(this).attr('href');
+
+						event.preventDefault();
+						event.stopPropagation();
+
+						// Hide.
+							$menu._hide();
+
+						// Redirect.
+							window.setTimeout(function() {
+								window.location.href = href;
+							}, 350);
+
+					});
+
+			$body
+				.on('click', 'a[href="#menu"]', function(event) {
+
+					event.stopPropagation();
+					event.preventDefault();
+
+					// Toggle.
+						$menu._toggle();
+
+				})
+				.on('keydown', function(event) {
+
+					// Hide on escape.
+						if (event.keyCode == 27)
+							$menu._hide();
+
+				});
+
+
+	});
+
+})(jQuery);
---
+++ src/Site/CustomisationExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Bundle\Site;
+
+use Bolt\Extension\SimpleExtension;
+use Silex\Application;
+use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Site bundle extension loader.
+ *
+ * This is the base bundle you can use to further customise Bolt for your
+ * specific site.
+ *
+ * It is perfectly safe to remove this bundle, just remember to remove the
+ * entry from your .bolt.yml or .bolt.php file.
+ *
+ * For more information on building bundles see https://docs.bolt.cm/extensions
+ */
+class CustomisationExtension extends SimpleExtension
+{
+    protected function registerFrontendRoutes(ControllerCollection $collection)
+    {
+        $collection->post('/mail', [$this, 'callbackMail'])->bind('contactmail');
+    }
+
+    public function callbackMail(Application $app, Request $request)
+    {
+        if ($request->request->get('name', '') && $request->request->get('email', '') && $request->request->get('message')) {
+            $name = $request->request->get('name', '');
+            $email = $request->request->get('email', '');
+            $message = $request->request->get('message');
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Nachricht vom Hilariverein')
+                    ->setFrom('info@hilariverein-langwiesen.ch')
+                    ->setTo('pascal.wacker@gmx.ch')
+                    ->setBody("Von: " . $name . "(" . $email . ")\n\r\n\rNachricht: " . $message)
+                    ->addPart("<strong>Von:</strong> " . $name . "(" . $email . ")<br /><br /><strong>Nachricht:</strong> " . $message, 'text/html');
+                $result = $app['mailer']->send($message);
+            }
+        }
+
+        return new RedirectResponse('/');
+    }
+}
